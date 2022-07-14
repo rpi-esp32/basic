299c7af5d48e00c94a54c5a502237e6d69f7cf37
MBA <rpi.esp32@gmail.com>
2022-07-14 21:59:43 +0800

    newest from yee on

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 // if ("serviceWorker" in navigator) {
 //     window.addEventListener("load", function() {
 //       navigator.serviceWorker
-//         .register("/basic/sw.js")
+//         .register("/sw.js")
 //         .then(res => console.log("service worker registered"))
 //         .catch(err => console.log("service worker not registered", err))
 //     })
 //   }
 
 
-h1 = document.getElementById('H1')
-hh1 = document.getElementById('HH1')
 
-h1.innerHTML = 'inside of h1 tag'
-hh1.innerHTML = 'inside of h2 tag'
+
+
 
 const doMain = () => {
 
