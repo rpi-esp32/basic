9088f7adc6724b97f6f7a0067becdf00a8ca5bac
MBA <rpi.esp32@gmail.com>
2022-07-14 21:05:32 +0800

    style

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 // if ("serviceWorker" in navigator) {
 //     window.addEventListener("load", function() {
 //       navigator.serviceWorker
-//         .register("/sw.js")
+//         .register("/basic/sw.js")
 //         .then(res => console.log("service worker registered"))
 //         .catch(err => console.log("service worker not registered", err))
 //     })
 //   }
 
 
+h1 = document.getElementById('H1')
+hh1 = document.getElementById('HH1')
 
-
-
+h1.innerHTML = 'inside of h1 tag'
+hh1.innerHTML = 'inside of h2 tag'
 
 const doMain = () => {
 
